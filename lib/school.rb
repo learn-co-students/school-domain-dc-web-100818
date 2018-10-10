e411d265c57298d82dc211039c2ba7cd35b09fe0
Drewisatlas <drewisatlas@gmail.com>
2018-10-10 18:53:26 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+require 'pry'
+
+class School
+  def initialize (name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student (name, grade)
+    if roster.key?(grade)
+      roster[grade] << name
+    else
+      roster[grade] = [name]
+    end
+  end
+
+  def grade (number)
+    @roster[number]
+  end
+
+  def sort
+    self.roster.each do |grade, students|
+      roster[grade] = students.sort
+    end
+  end
+end

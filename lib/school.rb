2914df02cb536bcb97aaf0197153877b581663a5
Joseph Arias <arias.joseph.a@gmail.com>
2018-10-11 02:05:16 -0400

    OO School Domain

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+class School 
+  def initialize(school_name) 
+    @name = school_name
+    @roster = {}
+  end 
+
+  def roster
+    @roster
+  end
+
+  def add_student(student_name, grade)
+    @student_name = student_name
+    @grade = grade
+    if @roster.include?(grade) == false
+      @roster[grade] = []
+    end
+    @roster[grade] << student_name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort 
+    @roster.each do |grade, names|
+      @roster[grade] = names.sort
+    end
+  end
+
+end
+
